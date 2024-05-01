93e1e4c79ce96673e8c2753c1772690d3b1d8cf1
finashin82 <finashin82@mail.ru>
2024-05-01 19:24:46 +0900

    Подготовил шаблон

--- PracticalWork_C_7.8/Repository.cs
+++ PracticalWork_C_7.8/Repository.cs
@@ -9,41 +9,11 @@ namespace PracticalWork_C_7._8
 {
     class Repository
     {
-        // Имя файла
-        string nameFileWorker = "Worker.csv";
-
         public Worker[] GetAllWorkers()
         {
             // здесь происходит чтение из файла
             // и возврат массива считанных экземпляров
 
-            
-
-            // Проверяем файл на существование и если он существует, то выводим данные
-            if (File.Exists(nameFileWorker))
-            {
-                // Открывает доступ к файлу для чтения данных
-                using (StreamReader sr = new StreamReader("Worker.txt", Encoding.Unicode))
-                {
-                    Console.WriteLine();
-
-                    Console.WriteLine();
-
-                    Console.WriteLine();
-                    string line;
-
-                    // Пока не пустая строка, выводим данные
-                    while ((line = sr.ReadLine()) != null)
-                    {
-                        // Разбиваем строку в массив по #
-                        string[] data = line.Split('#');
-                        Console.WriteLine($"{data[0],5} * {data[1],11} * {data[2],30} * {data[3],7} * " +
-                                            $"{data[4],4} * {data[5],13} * {data[6],15}");
-                    }
-                    Console.WriteLine();
-                }
-            }
-            else Console.WriteLine("Файл не найден.");
         }
 
         public Worker GetWorkerById(int id)
